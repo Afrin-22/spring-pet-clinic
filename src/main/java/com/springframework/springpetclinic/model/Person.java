629c26f8de7bd2aaac1bb8967c46f92c1041f57c
Afrin-22 <58210796+Afrin-22@users.noreply.github.com>
2021-03-14 16:20:28 +0530

    adding POJOs. Closes #2.

---
+++ src/main/java/com/springframework/springpetclinic/model/Person.java
@@ -0,0 +1,31 @@
+package com.springframework.springpetclinic.model;
+
+public class Person {
+
+	private String firstName;
+	
+	private String lastName;
+
+	
+	public Person() {
+		super();
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
