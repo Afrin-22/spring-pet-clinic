0724e2ee1189bde2f74401e4172dd76abdfffe91
Afrin-22 <58210796+Afrin-22@users.noreply.github.com>
2021-03-16 12:05:05 +0530

    refactoring to multimodule maven project. closes #1

---
+++ pet-clinic-data/src/main/java/org/springframework/petclinicdata/model/Person.java
@@ -0,0 +1,31 @@
+package org.springframework.petclinicdata.model;
+
+public class Person {
+
+	private String firstName;
+	
+	private String lastName;
+
+	
+	public Person() {
+		super();
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
