cdd1b8afd4edc61d130045ea0f2f674a149ef6ac
Afrin-22 <58210796+Afrin-22@users.noreply.github.com>
2021-03-24 20:38:11 +0530

    automatic ID generation in Map. closes #23

--- pet-clinic-web/src/main/java/org/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -27,13 +27,11 @@ public class DataLoader implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setFirstName("Eren");
 		owner1.setLastName("Jeager");
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setFirstName("Levi");
 		owner2.setLastName("Ackerman");
 		ownerService.save(owner2);
@@ -41,13 +39,11 @@ public class DataLoader implements CommandLineRunner {
 		System.out.println("Loaded owner...");
 		
 		Vet vet1 = new Vet();
-		vet1.setId(2L);
 		vet1.setFirstName("Erwin");
 		vet1.setLastName("Daijo");
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
 		vet2.setFirstName("Mikasa");
 		vet2.setLastName("Ackerman");
 		vetService.save(vet2);
