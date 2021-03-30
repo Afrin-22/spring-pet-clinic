81e9b0a9ffe521c2e3c7987ab21fc41a1f7097c8
Afrin-22 <58210796+Afrin-22@users.noreply.github.com>
2021-03-30 09:55:34 +0530

    added Speciality Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/org/springframework/springpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,37 @@
+package org.springframework.springpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.springpetclinic.model.Speciality;
+import org.springframework.springpetclinic.services.SpecialityService;
+
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService{
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		super.delete(object);
+	}
+	
+	
+
+}
