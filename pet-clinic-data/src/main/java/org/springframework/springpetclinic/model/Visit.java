dfe787cc7217220043341175874ce964c481eee1
Afrin-22 <58210796+Afrin-22@users.noreply.github.com>
2021-04-05 17:00:05 +0530

    added visit entity. closes #38

--- pet-clinic-data/src/main/java/org/springframework/springpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/org/springframework/springpetclinic/model/Visit.java
@@ -2,10 +2,24 @@ package org.springframework.springpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="visits")
 public class Visit extends BaseEntity{
 	
+	@Column(name="local_date")
 	private LocalDate localDate;
+	
+	@Column(name="description")
 	private String description;
+
+	@ManyToOne
+	@JoinColumn(name="pet_id")
 	private Pet pet;
 	
 	public LocalDate getLocalDate() {
@@ -27,6 +41,4 @@ public class Visit extends BaseEntity{
 		this.pet = pet;
 	}
 	
-	
-
 }
